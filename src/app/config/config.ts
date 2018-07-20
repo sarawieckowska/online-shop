8e09eae17b83c00d00e32f62b31538ea42184443
saraW <sara.Wieckowskaa@gmail.com>
2018-07-20 21:41:59 +0200

    header: categories button

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,7 +1,7 @@
 enum mantle {
-    login = 'http://192.168.0.5:3000/user/login',
+    login = 'http://192.168.0.3:3000/user/login',
     createAccount = 'http://localhost:3000/user/create',
-    categoriesList = 'http://192.168.0.5:3000/categories/list'
+    categoriesList = 'http://192.168.0.3:3000/categories/list'
 }
 
 export default {
